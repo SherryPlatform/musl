dc54d8e53d06f65856802c02c324c66478d18762
MouriNaruto <Kenji.Mouri@outlook.com>
2025-03-21 16:14:29 +0800

    workaround the issue that pthread_getattr_np won't return in FreeBSD Linuxulator
    Some notes from Kenji Mouri (MouriNaruto) about this workaround:
    
    I tried to use musl-based userspace in FreeBSD's Linuxulator, I found some
    Node.js based app will hung at startup. I did some investigations and finally
    found that caused by the mremap loop in the musl's pthread_getattr_np. I read
    the FreeBSD's Linuxulator source code, it seems mremap in FreeBSD's Linuxulator
    in won't support expand the size.
    
    With Edward Tomasz Napierala (https://wiki.freebsd.org/EdwardTomaszNapierala)'s
    suggestion, choose to use RLIMIT_STACK _in addition_ to the current measurement
    can workaround the issue without doing much modifications.
    
    Consider that getrlimit may failed, so I think we should use __default_stacksize
    when getrlimit failed.
    
    I have tested this commit in my FreeBSD environment with Visual Studio Code
    Remote SSH server which requires the FreeBSD's Linuxulator.

--- src/thread/pthread_getattr_np.c
+++ src/thread/pthread_getattr_np.c
@@ -2,6 +2,7 @@
 #include "pthread_impl.h"
 #include "libc.h"
 #include <sys/mman.h>
+#include <sys/resource.h>
 
 int pthread_getattr_np(pthread_t t, pthread_attr_t *a)
 {
@@ -12,11 +13,19 @@ int pthread_getattr_np(pthread_t t, pthread_attr_t *a)
 		a->_a_stackaddr = (uintptr_t)t->stack;
 		a->_a_stacksize = t->stack_size;
 	} else {
+		struct rlimit rl_stack = {0};
+		/* get the current stack limit to workaround the issue */
+		/* that mremap loop won't stop in FreeBSD Linuxulator */
+		if (getrlimit(RLIMIT_STACK, &rl_stack) != 0) {
+			/* use default stack size if getrlimit fails */
+			rl_stack.rlim_cur = __default_stacksize;
+			rl_stack.rlim_max = __default_stacksize;
+		}
 		char *p = (void *)libc.auxv;
 		size_t l = PAGE_SIZE;
 		p += -(uintptr_t)p & PAGE_SIZE-1;
 		a->_a_stackaddr = (uintptr_t)p;
-		while (mremap(p-l-PAGE_SIZE, PAGE_SIZE, 2*PAGE_SIZE, 0)==MAP_FAILED && errno==ENOMEM)
+		while (l <= rl_stack.rlim_cur && mremap(p-l-PAGE_SIZE, PAGE_SIZE, 2*PAGE_SIZE, 0)==MAP_FAILED && errno==ENOMEM)
 			l += PAGE_SIZE;
 		a->_a_stacksize = l;
 	}
